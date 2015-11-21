56bcb63816572c88d6749be8038ac2e4c4bfe96a
asif <asif@asshi-PC>
2015-11-21 22:29:36 +0500

    Popup with Editor and also email send functionality and X button added and their functionality.

--- docs/partner_platform/example_app_ipp_v3/example_invoice_query.php
+++ docs/partner_platform/example_app_ipp_v3/example_invoice_query.php
@@ -4,72 +4,133 @@ require_once dirname(__FILE__) . '/config.php';
 
 require_once dirname(__FILE__) . '/views/header.tpl.php';
 
+
+$conn = new mysqli("localhost","root","","ezb2599b_qb");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+if(isset($_REQUEST['invoice'])){
+	$UpdateStatus_sql = "UPDATE quickbooks_invoice SET invoice_status = 1 WHERE invoice_num = ".$_REQUEST['invoice']."";
+	$conn->query($UpdateStatus_sql);
+	header('Location:'.$actual_link.'');
+}
+
+if(isset($_REQUEST['send_email'])){
+	// To send HTML mail, the Content-type header must be set
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	
+	// Additional headers
+	$headers .= 'To: <'.$_REQUEST['to'].'>' . "\r\n";
+	$headers .= 'From: <'.$_REQUEST['from'].'>' . "\r\n";
+	
+	// Mail it
+	mail($_REQUEST['to'], $_REQUEST['subject'], $_REQUEST['message'], $headers);
+}
+
 ?>
-<div class="row">
-    <div class="col-md-12">
-        <h1>Invoices from QB Live Application</h1>
-    </div>
-    
+<script type="text/javascript">
+tinymce.init({
+    selector: "textarea#message",
+    plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+});
+</script>
+<div id="contactdiv">
+  <form class="form" method="post" action="" id="contact">
+    <img src="css/button_cancel.png" class="img" id="cancel"/>
+    <h3>Send E-mail</h3>
+    <hr/>
+    <br/>
+    <label>To: <span>*</span></label>
+    <br/>
+    <input type="text" id="to" name="to" placeholder="To"/>
+    <br/>
+    <br/>
+    <label>From: <span>*</span></label>
+    <br/>
+    <input type="text" id="from" name="from" placeholder="From"/>
+    <br/>
+    <br/>
+    <label>Subject: <span>*</span></label>
+    <br/>
+    <input type="text" id="subject" name="subject" placeholder="Subject" value="Your order has been shipped! Order Number# [INVOICE #]"/>
+    <br/>
+    <br/>
+    <label>Message:</label>
+    <br/>
+    <textarea id="message" name="message" placeholder="Message.......">
+    Hello [Client],
+    I am pleased to inform you that your order has finished printing! As stated in our earlier conversation, your order is being shipped via  [Shipping Method], which will take [dependant on shipping method] days.
+    Here are the details of your delivery.
+    [Shipping Address]
+    </textarea>
+    <br/>
+    <br/>
+    <input type="button" id="send" name="send_email" value="Send"/>
+    <input type="button" id="cancel" value="Cancel"/>
+    <br/>
+  </form>
 </div>
 <div class="row">
-    <div class="col-md-12">
-        <hr>
-    </div>
-    
+  <div class="col-md-12">
+    <h1>Invoices from QB Live Application</h1>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <hr>
+  </div>
 </div>
 <table class="table-striped table">
-    <tr>
-        <td>
-            Internal ID
-        </td>
-        <td>
-            Invoice#
-        </td>
-        <td>
-            Amount
-        </td>
-        <td>
-            Details
-        </td>
-        <td>
-             Actions
-        </td>
-    </tr>
-
-
-<?php
+  <tr>
+    <td> Internal ID </td>
+    <td> Invoice# </td>
+    <td> Amount </td>
+    <td> Details </td>
+    <td> Actions </td>
+  </tr>
+  <?php
 $InvoiceService = new QuickBooks_IPP_Service_Invoice();
 
 $invoices = $InvoiceService->query($Context, $realm, "SELECT * FROM Invoice STARTPOSITION 1 MAXRESULTS 10");
 //$invoices = $InvoiceService->query($Context, $realm, "SELECT * FROM Invoice WHERE DocNumber = '1002' ");
 
-//print_r($customers);
-
+# perform the local query....
+	$sql = "Select * from quickbooks_invoice";
+	$result = $conn->query($sql);
+	
+	$row = $result->fetch_assoc();
+	
 foreach ($invoices as $Invoice)
-{
-    ?>
-    <tr>
-        <td>
-             <?php echo $Invoice->getId();?>
-        </td>
-        <td>
-             <?php echo $Invoice->getDocNumber();?>
-        </td>
-        <td>
-            $<?php echo $Invoice->getTotalAmt();?>
-        </td>
-        <td>
-            <?php echo $Invoice->getLine(0)->getDescription();?>
-        </td>
-        <td>
-             <a class="btn btn-success btn-sm" href="#">Edit</a>&nbsp;<a class="btn btn-success btn-sm" href="#">Send Email</a>
-        </td>
-    </tr>
-    
-<?php 
+{	
+	if($Invoice->getDocNumber() != $row['invoice_num']){
+		$insert_sql = "INSERT INTO quickbooks_invoice (invoice_num,invoice_amount,invoice_status) VALUES('".$Invoice->getDocNumber()."','".$Invoice->getTotalAmt()."','0')";
+		$conn->query($insert_sql);
+	}
+
+?>
+  <tr>
+    <td><?php echo $Invoice->getId();?></td>
+    <td><?php echo $Invoice->getDocNumber();?></td>
+    <td> $<?php echo $Invoice->getTotalAmt();?></td>
+    <td><?php echo $Invoice->getLine(0)->getDescription();?></td>
+    <td><a class="btn btn-success btn-sm" href="#">Edit</a>&nbsp;<a class="btn btn-success btn-sm" id="send_email" href="javascript:void(0);">Send Email</a>
+    <a class="btn btn-success btn-sm" href="<?php echo $actual_link.'?invoice='.$Invoice->getDocNumber().'';?>">X</a>
+    </td>
+  </tr>
+  <?php 
 }
 ?>
-    </table>
+</table>
 <?php
 //print('Invoice # ' . $Invoice->getDocNumber() . ' has a total of $' . $Invoice->getTotalAmt() . "\n");
 //print('    First line item: ' . $Invoice->getLine(0)->getDescription() . "\n");
--- docs/partner_platform/example_app_ipp_v3/views/header.tpl.php
+++ docs/partner_platform/example_app_ipp_v3/views/header.tpl.php
@@ -1,59 +1,63 @@
 <html>
-	<head>
-		<title>IPP Test App</title>
+<head>
+<title>IPP Test App</title>
 
-		<!-- Every page of your app should have this snippet of Javascript in it, so that it can show the Blue Dot menu -->
-		<script type="text/javascript" src="https://appcenter.intuit.com/Content/IA/intuit.ipp.anywhere.js"></script>
-		<script type="text/javascript">
+<!-- Every page of your app should have this snippet of Javascript in it, so that it can show the Blue Dot menu -->
+<script type="text/javascript" src="https://appcenter.intuit.com/Content/IA/intuit.ipp.anywhere.js"></script>
+<script type="text/javascript">
 		intuit.ipp.anywhere.setup({
 			menuProxy: '<?php print($quickbooks_menu_url); ?>',
 			grantUrl: '<?php print($quickbooks_oauth_url); ?>'
 		});
 		</script>
+<style>
+table {
+	margin-left: 20px;
+	margin-right: 20px;
+}
+ tr:nth-child(even) {
+background: #CCC
+}
+ tr:nth-child(odd) {
+background: #EEE
+}
+td {
+	padding: 4px;
+}
+</style>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="views/bootstrap-3.3.5-dist/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
-		<style>
+<!-- Optional theme -->
+<link rel="stylesheet" href="views/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
 
-		table
-		{
-			margin-left: 20px;
-			margin-right: 20px;
-		}
+<!-- Latest compiled and minified JavaScript -->
+<script src="views/bootstrap-3.3.5-dist/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 
-		tr:nth-child(even) {background: #CCC}
-		tr:nth-child(odd) {background: #EEE}
+<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<link rel="stylesheet" href="css/popup.css" />
+<script src="js/popup.js"></script>
 
-		td
-		{
-			padding: 4px;
-		}
+<script src="js/tinymce/tinymce.min.js"></script>
 
-		</style>
-                <!-- Latest compiled and minified CSS -->
-                <link rel="stylesheet" href="views/bootstrap-3.3.5-dist/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-
-                <!-- Optional theme -->
-                <link rel="stylesheet" href="views/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
-
-                <!-- Latest compiled and minified JavaScript -->
-                <script src="views/bootstrap-3.3.5-dist/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
-	</head>
-	<body>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 pullright">
-                        <?php if ($quickbooks_is_connected): ?>
-                                <ipp:blueDot></ipp:blueDot>
-                        <?php endif; ?>
-                    </div>
-                </div>
-                <div class="row">
-                    <ul class = "nav navbar-nav">
-                       <li class = "active"><a href = "index.php">Home</a></li>
-                       <li><a href = "example_customer_query.php">Customers</a></li>
-                       <li><a href = "example_invoice_query.php">Invoices</a></li>
-                       <li><a href = "example_invoice_w_lines_query.php">Invoices with Lines</a></li>
-                       <li><a href = "example_payment_query.php">Payments</a></li>
-                       <li><a href = "example_items_query.php">Items</a></li>
-                       
-                    </ul>
-                 </div>
+</head>
+<body>
+<div class="container">
+<div class="row">
+  <div class="col-md-12 pullright">
+    <?php if ($quickbooks_is_connected): ?>
+    <ipp:blueDot></ipp:blueDot>
+    <?php endif; ?>
+  </div>
+</div>
+<div class="row">
+  <ul class = "nav navbar-nav">
+    <li class = "active"><a href = "index.php">Home</a></li>
+    <li><a href = "example_customer_query.php">Customers</a></li>
+    <li><a href = "example_invoice_query.php">Invoices</a></li>
+    <li><a href = "example_invoice_w_lines_query.php">Invoices with Lines</a></li>
+    <li><a href = "example_payment_query.php">Payments</a></li>
+    <li><a href = "example_items_query.php">Items</a></li>
+  </ul>
+</div>
