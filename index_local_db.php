7909b631916dec7f9a548c359178ecc13bbf7a85
Muhammad Nauman <mhmmd.nauman@gmail.com>
2015-12-23 15:01:19 +0500

    sobia fixes of invoices with local db

--- index_local_db.php
+++ index_local_db.php
@@ -19,8 +19,8 @@ $objInvoice = new Invoice();
         <script src = "https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src = "https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-          <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-            <script type="text/javascript" src="jquery.sticky.js"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script type="text/javascript" src="jquery.sticky.js"></script>
         <style type="text/css">
             .big-drop-down{ width: 120px;}
             .small-drop-down{ width: 80px;}
